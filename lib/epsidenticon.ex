6828be2c6d3bf9b425cfd923026f7bf35354c8b2
Altin Thaci <altin.thaci@fresha.com>
2024-04-23 22:07:35 +0200

    build grid and mirror rows

--- lib/epsidenticon.ex
+++ lib/epsidenticon.ex
@@ -3,6 +3,7 @@ defmodule Epsidenticon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -15,5 +16,21 @@ defmodule Epsidenticon do
 
   def pick_color(%Epsidenticon.Image{hex: [r, g, b | _tail]} = image) do
     %Epsidenticon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Epsidenticon.Image{hex: hex_list}) do
+    hex_list
+    |> Enum.chunk_every(3)
+    |> Enum.map(&mirrow_row/1)
+  end
+
+  def mirrow_row(row) do
+    case row do
+      [first, second | _tail] ->
+        row ++ [second, first]
+
+      _ ->
+        nil
+    end
   end
 end
